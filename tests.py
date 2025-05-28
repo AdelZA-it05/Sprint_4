86e7a3c0f020de3db682e6dcf02320aca5e3fb6d
Adel Aydamirov <a.aydamirov70@yandex.ru>
2025-05-28 12:49:14 +0300

    Исправления по замечаниям (Sprint_4)

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import pytest
 
 from main import BooksCollector
 
+
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
@@ -10,57 +11,46 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self, add_instance_collector):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
         # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        add_instance_collector.add_new_book('Гордость и предубеждение и зомби')
+        add_instance_collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.books_genre) == 2
+        assert len(add_instance_collector.books_genre) == 2
 
     @pytest.mark.parametrize(
         'book',
         ['Что делать, если ваш кот хочет вас убить']
     )
-    def test_add_new_book_add_one_book(self, book):
-        collector = BooksCollector()
-        collector.add_new_book(book)
-        assert len(collector.books_genre) == 1
-
-    def test_set_book_genre_add_correct(self, add_one_sciencefiction_book):        
-        assert add_one_sciencefiction_book.books_genre.get('Что делать, если ваш кот хочет вас убить') == 'Фантастика'
+    def test_add_new_book_add_one_book(self, add_instance_collector, book):
+        add_instance_collector.add_new_book(book)
+        assert len(add_instance_collector.books_genre) == 1
 
     @pytest.mark.parametrize(
         'book, genre',
         [
-            ['Что делать, если ваш кот хочет вас убить', 'Фантастика'],
-            ['Собака баскервилей', 'Ужасы'],
-            ['Мой личный враг', 'Детективы'],
-            ['Ну Погоди!', 'Мультфильмы'],
-            ['Бриллиантовая рука', 'Комедии']
+            ['Что делать, если ваш кот хочет вас убить', 'Фантастика']
         ]
     )
-    def test_set_book_genre_add_correct1(self, book, genre):
-        collector = BooksCollector()
-        collector.add_new_book(book)
-        collector.set_book_genre(book, genre)
-        assert collector.books_genre.get(book) == genre
+    def test_set_book_genre_add_correct(self, add_instance_collector, book, genre):
+        add_instance_collector.add_new_book(book)
+        add_instance_collector.set_book_genre(book, genre)
+        assert add_instance_collector.get_book_genre(book) == genre
 
-    def test_get_book_genre_from_another_book(self, add_one_sciencefiction_book):
-        add_one_sciencefiction_book.add_new_book('Звёздные войны')
-        add_one_sciencefiction_book.set_book_genre('Звёздные войны', add_one_sciencefiction_book.get_book_genre('Что делать, если ваш кот хочет вас убить'))
-        assert add_one_sciencefiction_book.books_genre.get('Звёздные войны') == 'Фантастика'
+    def test_get_book_genre_from_another_book(self, add_instance_collector, add_one_sciencefiction_book):
+        add_instance_collector.add_new_book('Трудно быть богом')
+        add_instance_collector.set_book_genre('Трудно быть богом', add_one_sciencefiction_book.get_book_genre('Что делать, если ваш кот хочет вас убить'))
+        assert add_instance_collector.get_book_genre('Трудно быть богом') == 'Фантастика'
 
 
     def test_get_books_with_specific_genre_one(self, add_one_book_every_genre):
         assert add_one_book_every_genre.get_books_with_specific_genre('Ужасы') == ['Собака баскервилей']
 
     def test_get_books_genre_dict_value(self, add_one_book_every_genre):
-        assert add_one_book_every_genre.books_genre == {
+        assert add_one_book_every_genre.get_books_genre() == {
     'Что делать, если ваш кот хочет вас убить': 'Фантастика'
     , 'Собака баскервилей': 'Ужасы'
     , 'Мой личный враг': 'Детективы'
@@ -84,5 +74,6 @@ class TestBooksCollector:
     def test_get_list_of_favorites_books_three_list(self, add_one_book_every_genre):
         add_one_book_every_genre.add_book_in_favorites('Бриллиантовая рука')
         add_one_book_every_genre.add_book_in_favorites('Собака баскервилей')
+        add_one_book_every_genre.add_book_in_favorites('Ну Погоди!')
         add_one_book_every_genre.delete_book_from_favorites('Ну Погоди!')
-        assert len(add_one_book_every_genre.favorites) == 2
+        assert len(add_one_book_every_genre.get_list_of_favorites_books()) == 2
